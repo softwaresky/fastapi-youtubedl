19b2a6be7effdd0636a225cfe9cc9ff97a03a751
mihail-sofeski <mihail@loka.com>
2021-02-19 21:54:51 +0100

    update

--- frontend/src/views/ydl-item/YdlItemsView.vue
+++ frontend/src/views/ydl-item/YdlItemsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h4>asdasdasd</h4>
+    <h4>List of download items</h4>
     <data-tables
         :data="allYdlItems"
         :total="allYdlItems.length"
@@ -13,6 +13,7 @@
       <el-table-column
           label="Status"
           prop="status"
+          :formatter="columnFormatter"
           sortable
       />
       <el-table-column
@@ -23,7 +24,13 @@
           label="Timestamp"
           prop="timestamp"
           sortable
-      />
+
+      >
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ columnFormatter(scope.row, scope.column) }} </span>
+        </template>
+      </el-table-column>
       <el-table-column
           label="Add. Options"
           prop="ydl_opts"
@@ -37,16 +44,16 @@
 import {Component, Vue} from "vue-property-decorator";
 import {dispatchCreateYdlItem, dispatchGetYdlItems, dispatchUpdateYdlItem} from '@/store/ytdl_item/actions';
 import {readAllYtdItems} from '@/store/ytdl_item/getters';
+import moment from 'moment';
 
-/*
-    id = Column(Integer, primary_key=True, index=True)
-    url = Column(String)
-    do_calculate_pattern = Column(Boolean, default=False)
-    status = Column(Integer)
-    timestamp = Column(DateTime, default=datetime.datetime.now)
-    ydl_opts = Column(JSON)
-    output_log = Column(JSON)
- */
+enum statusName {
+
+  'Not Found' = 0,
+  Pending = 1,
+  Running = 2,
+  Error = 3,
+  Finished = 4,
+}
 
 @Component
 export default class YdlItemsView extends Vue {
@@ -57,6 +64,25 @@ export default class YdlItemsView extends Vue {
 
   get allYdlItems() {
     return readAllYtdItems(this.$store);
+  }
+
+  public columnFormatter(row: any, column: any) {
+
+    const cellValue = row[column.property];
+
+    if (column.property.includes('time')) {
+      if (cellValue) {
+        return moment(cellValue).format('MM/DD/YY HH:mm');
+      } else if (column.property == 'elapsed_time' && row['start_time'] && row['end_time']) {
+        return moment.utc(moment(row['end_time']).diff(moment(row['start_time']))).format('HH:mm:ss');
+      } else {
+        return ' N/A';
+      }
+    } else if (column.property === 'status') {
+      return statusName[cellValue];
+    }
+
+    return cellValue;
   }
 }
 </script>
