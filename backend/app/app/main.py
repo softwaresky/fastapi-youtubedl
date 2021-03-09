633d77f34e47bb2d1a0c3356c5eac8b41415a578
mihail-sofeski <mihail@loka.com>
2021-03-09 23:52:57 +0100

    organized files and fix

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse, JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
@@ -9,11 +10,19 @@ from app.core.config import settings
 # app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
-app.mount("/static", StaticFiles(directory="/vue/dist"), name="static")
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
-@app.get("/", include_in_schema=False)
-def root():
-    with open('/vue/dist/index.html') as f:
-        return HTMLResponse(content=f.read(), status_code=200)
+# Mounting default Vue files after running npm run build
+app.mount("/dist", StaticFiles(directory="/vue/dist/"), name="dist")
+app.mount("/css", StaticFiles(directory="/vue/dist/css"), name="css")
+app.mount("/fonts", StaticFiles(directory="/vue/dist/fonts"), name="fonts")
+# app.mount("/img", StaticFiles(directory="/vue/dist/img"), name="img")
+app.mount("/js", StaticFiles(directory="/vue/dist/js"), name="js")
+
+# app.mount("/static", StaticFiles(directory="/vue/dist"), name="static")
+
+templates = Jinja2Templates(directory="/vue/dist")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
