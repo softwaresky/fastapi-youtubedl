38000c24847037cd8e3e88d6b87181bf7d4ae7b7
mihail-sofeski <mihail@loka.com>
2021-03-17 23:11:23 +0100

    update

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,56 +1,36 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse, JSONResponse, HTMLResponse, FileResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.templating import Jinja2Templates
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 import os
 
-# app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+folder = "C:/Users/Softwaresky/PycharmProjects/fastapi-youtubedl/frontend/dist"
+app.mount("/static", StaticFiles(directory=folder), name="static")
+
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-# # Mounting default Vue files after running npm run build
-# app.mount("/dist", StaticFiles(directory="/vue/dist/"), name="dist")
-# app.mount("/css", StaticFiles(directory="/vue/dist/css"), name="css")
-# app.mount("/fonts", StaticFiles(directory="/vue/dist/fonts"), name="fonts")
-# # app.mount("/img", StaticFiles(directory="/vue/dist/img"), name="img")
-# app.mount("/js", StaticFiles(directory="/vue/dist/js"), name="js")
-
-folder = "/vue/dist"
-app.mount("/static", StaticFiles(directory="/vue/dist"), name="static")
-
-# templates = Jinja2Templates(directory="/vue/dist")
-#
-# @app.get("/", response_class=HTMLResponse)
-# async def root(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
-
-
 
 @app.get("/", response_class=FileResponse)
 def read_index(request: Request):
     path = f'{folder}/index.html'
     return FileResponse(path)
 
-@app.get("/.*", include_in_schema=False)
-def root():
-    path = f'{folder}/index.html'
-    return FileResponse(path)
-
-
-@app.get("/{catchall:path}", response_class=FileResponse)
-def read_index(request: Request):
-    # check first if requested file exists
-    path = request.path_params["catchall"]
-    file = f"{folder}/{path}"
-
-    print('look for: ', path, file, os.path.exists(file))
-    if os.path.exists(file):
-        return FileResponse(file)
-
-    # otherwise return index files
-    index = f'{folder}/index.html'
-    return FileResponse(index)
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print ("custom_http_exception_handler() ....")
+    return RedirectResponse("/")
--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,5 +1,5 @@
 const env = process.env.VUE_APP_ENV;
 
 // export const apiUrl = `http://${process.env.VUE_APP_DOMAIN}`;
-export const apiUrl = `${process.env.VUE_APP_DOMAIN}`;
+export const apiUrl = `http://127.0.0.1:8765`;
 export const appName = process.env.VUE_APP_NAME;
