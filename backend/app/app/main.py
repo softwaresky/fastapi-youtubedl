72244a14bdcedc505cf0d292d5817cabbfac2ad1
mihail-sofeski <mihail@loka.com>
2021-04-04 20:46:21 +0200

    update for docker static env

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 from starlette.responses import JSONResponse, HTMLResponse, FileResponse
 from app.api.api_v1.api import api_router
@@ -6,13 +7,17 @@ from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
-
-folder = "/vue"
-app.mount("/static", StaticFiles(directory=folder), name="static")
+app.add_middleware(CORSMiddleware,
+               allow_origins=["*"],
+               allow_methods="*")
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
-@app.get("/", response_class=FileResponse)
-def root():
-    path = f'{folder}/index.html'
-    return FileResponse(path, media_type='text/html')
+if settings.STATIC_FOLDER:
+    folder = settings.STATIC_FOLDER
+    app.mount("/static", StaticFiles(directory=folder), name="static")
+
+    @app.get("/", response_class=FileResponse)
+    def root():
+        path = f'{folder}/index.html'
+        return FileResponse(path, media_type='text/html')
