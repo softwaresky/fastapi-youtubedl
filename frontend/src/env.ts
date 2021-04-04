1e90c21f11a16753ad4117f607cc670a5ada8984
mihail-sofeski <mihail@loka.com>
2021-04-04 20:22:11 +0200

    remove unused component, add new input field pattern in new ydl item and fix env.ts

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -3,15 +3,16 @@ const env = process.env.VUE_APP_ENV;
 let envApiUrl = '';
 
 if (env === 'production') {
-    envApiUrl = process.env.VUE_APP_DOMAIN_PROD == undefined ? `https://${process.env.VUE_APP_DOMAIN_PROD}` : '';
+    envApiUrl = process.env.VUE_APP_DOMAIN_PROD == undefined ? '' : `https://${process.env.VUE_APP_DOMAIN_PROD}`;
 } else if (env === 'staging') {
-    envApiUrl = process.env.VUE_APP_DOMAIN_STAG == undefined ? `https://${process.env.VUE_APP_DOMAIN_STAG}` : '';
+    envApiUrl = process.env.VUE_APP_DOMAIN_STAG == undefined ? '' : `https://${process.env.VUE_APP_DOMAIN_STAG}`;
 } else if (env === 'development') {
-    envApiUrl = process.env.VUE_APP_DOMAIN_DEV == undefined ? `http://${process.env.VUE_APP_DOMAIN_DEV}` : '';
-} else if (env === 'local') {
+    envApiUrl = process.env.VUE_APP_DOMAIN_DEV == undefined ? '' : `http://${process.env.VUE_APP_DOMAIN_DEV}`;
+} else if (env === 'static') {
     envApiUrl = '';
 } else {
-    envApiUrl = process.env.VUE_APP_DOMAIN == undefined ? `http://${process.env.VUE_APP_DOMAIN}` : '';
+    // envApiUrl = process.env.VUE_APP_DOMAIN == undefined ? '' : `http://${process.env.VUE_APP_DOMAIN}`;
+    envApiUrl = process.env.VUE_APP_DOMAIN == undefined ? '' : `${process.env.VUE_APP_DOMAIN}`;
 }
 
 export const apiUrl = envApiUrl;
